82e9ad22f8686c189829b12652e15c24f6d96870
Taranjeet Singh <58724892+TaranjeetSingh121@users.noreply.github.com>
2022-06-14 07:09:36 -0700

    Add files via upload

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='accenc',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Accent encoder using speaker ver embeds',
+    author='amsterg',
+    license='MIT',
+)
